59878517d972d657f6ca4505a44f60026f69d06b
abenhansen <abenhansen@gmail.com>
2022-04-26 14:03:47 +0200

    Converet Hello to API

--- src/main/java/basics/helloworld/action/HelloController.java
+++ src/main/java/basics/helloworld/action/HelloController.java
@@ -1,6 +1,5 @@
 package basics.helloworld.action;
 
-import com.opensymphony.xwork2.ActionSupport;
 import basics.helloworld.model.MessageStore;
 import com.opensymphony.xwork2.ModelDriven;
 import com.opensymphony.xwork2.interceptor.ParameterNameAware;
@@ -15,21 +14,29 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-public class HelloController extends RestActionSupport implements SessionAware, ParameterNameAware {
-    private MessageStore messageStore;
+public class HelloController extends RestActionSupport implements SessionAware, ParameterNameAware, ModelDriven<MessageStore> {
+
+    private MessageStore model = new MessageStore();
+    ;
     private String userName;
     private static int helloCount = 0;
-    private Map<String, Object> userSession ;
+    private Map<String, Object> userSession;
     private static final String HELLO_COUNT = "helloCount";
     private static final Logger log = LogManager.getLogger(HelloController.class);
 
 
     public HttpHeaders index() {
         log.debug("In execute method of class Hello");
-        messageStore = new MessageStore();
+        //MessageStore newStore = new MessageStore();
+        String message = model.getMessage();
         if (userName != null) {
-            messageStore.setMessage( messageStore.getMessage() + " " + userName);
+            model.setMessage(" " + userName);
         }
+        if (userSession.get(HELLO_COUNT) != null)
+            message += "\n I've said hello " + userSession.get(HELLO_COUNT) + " times!";
+        model.setMessage(message);
+
+        //model = newStore;
         increaseHelloCount();
         return new DefaultHttpHeaders("index");
     }
@@ -37,7 +44,7 @@ public class HelloController extends RestActionSupport implements SessionAware, 
 
     private void increaseHelloCount() {
         Integer helloCount = (Integer) userSession.get(HELLO_COUNT);
-        if (helloCount == null ) {
+        if (helloCount == null) {
             helloCount = 1;
         } else {
             helloCount++;
@@ -47,22 +54,19 @@ public class HelloController extends RestActionSupport implements SessionAware, 
 
     @Override
     public void setSession(Map<String, Object> session) {
-        userSession = session ;
+        userSession = session;
     }
 
     public boolean acceptableParameterName(String parameterName) {
-        boolean allowedParameterName = true ;
+        boolean allowedParameterName = true;
 
-        if ( parameterName.contains("session")  || parameterName.contains("request") ) {
-            allowedParameterName = false ;
+        if (parameterName.contains("session") || parameterName.contains("request")) {
+            allowedParameterName = false;
         }
 
         return allowedParameterName;
     }
 
-    public MessageStore getMessageStore() {
-        return messageStore;
-    }
     public int getHelloCount() {
         return helloCount;
     }
@@ -73,5 +77,14 @@ public class HelloController extends RestActionSupport implements SessionAware, 
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public void setModel(MessageStore model) {
+        this.model = model;
+    }
+
+    @Override
+    public MessageStore getModel() {
+        return model;
     }
 }
