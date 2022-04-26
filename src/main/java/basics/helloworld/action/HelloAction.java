705ea4245b0150120ee6390f2c589a829851907f
abenhansen <abenhansen@gmail.com>
2022-04-26 09:55:28 +0200

    Update Annotation version with rest of the turtorial

--- src/main/java/basics/helloworld/action/HelloAction.java
+++ src/main/java/basics/helloworld/action/HelloAction.java
@@ -2,14 +2,19 @@ package basics.helloworld.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import basics.helloworld.model.MessageStore;
+import com.opensymphony.xwork2.interceptor.ParameterNameAware;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.struts2.interceptor.SessionAware;
 
-public class HelloAction extends ActionSupport {
+import java.util.Map;
+
+public class HelloAction extends ActionSupport implements SessionAware, ParameterNameAware {
     private MessageStore messageStore;
     private String userName;
     private static int helloCount = 0;
-
+    private Map<String, Object> userSession ;
+    private static final String HELLO_COUNT = "helloCount";
     private static final Logger log = LogManager.getLogger(HelloAction.class);
 
 
@@ -19,8 +24,34 @@ public class HelloAction extends ActionSupport {
         if (userName != null) {
             messageStore.setMessage( messageStore.getMessage() + " " + userName);
         }
-        helloCount++;
+        increaseHelloCount();
         return SUCCESS;
+    }
+
+
+    private void increaseHelloCount() {
+        Integer helloCount = (Integer) userSession.get(HELLO_COUNT);
+        if (helloCount == null ) {
+            helloCount = 1;
+        } else {
+            helloCount++;
+        }
+        userSession.put(HELLO_COUNT, helloCount);
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        userSession = session ;
+    }
+
+    public boolean acceptableParameterName(String parameterName) {
+        boolean allowedParameterName = true ;
+
+        if ( parameterName.contains("session")  || parameterName.contains("request") ) {
+            allowedParameterName = false ;
+        }
+
+        return allowedParameterName;
     }
 
     public MessageStore getMessageStore() {
